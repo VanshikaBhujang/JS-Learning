7275d96873f8eac594d652b98d49d2c4ee731390
Vanshika Bhujang <90775662+VanshikaBhujang@users.noreply.github.com>
2024-07-13 22:56:48 +0000

    functions and js file execution

--- Basics_2/functions.js
+++ Basics_2/functions.js
@@ -6,18 +6,78 @@ function sayMyName(){
 sayMyName(); // --> function execution
 // sayMyName --> function reference
 
+console.log(addNumbers(200, 30, 5));  // function 'addNumbers' can be called before its definition
 function addNumbers(...num){ // ... is called rest/spread operator, rest in the current case
     return num               // rest operator collects the scattered values and combines them into a list
 }
-console.log(addNumbers(200, 30, 5));
+
 
 //passing an object to a function
 function func1(obj){
     return `Username is ${obj.username} and password is ${obj.password}`
 }
-
-res = func1({
+const res = func1({
     "username": "vanshika",
     "password": "qwerty123"
 })
-console.log(res);
+
+// console.log(resp(3));   // below function cannot be called before its initialization because its a variable
+const resp = function(num){
+    return num +2 
+}
+console.log(resp(3));
+
+
+// ******** this keyword and Arrow functions ***********
+const obj1 = {
+    username: "vanshika",
+    welcomeMessage: function(){
+        console.log(`${this.username}`); // this refers to current object ( obj1)
+        console.log(this);
+    }
+}
+obj1.welcomeMessage()
+obj1.username = "vedika"
+obj1.welcomeMessage()
+console.log(this); // this is an empty object here since we're now in node env although if you try to console log this in 
+// browser then it'll give window object as output since it's the global object in browser
+
+function chai1(){
+    const username = "kimberly"
+    console.log(this.username); //using this keyword just in a function which does not belong to any object won't work
+    // it will result as undefined
+}
+
+const response = function chai2(){
+    const username = "kimberly"
+    console.log(this.username); //using this keyword just in a function which does not belong to any object won't work
+    // it will result as undefined
+}
+
+// Arrow function
+const result = () => {
+    const username = "kimberly"
+    console.log(this.username); //using this keyword just in a function which does not belong to any object won't work
+    // it will result as empty object
+}
+
+// Implicit return basically one liner function without return keyword and curly braces
+const addTwoNumbers = (num1, num2) => (num1 + num2)
+console.log(addTwoNumbers(2, 1));
+
+
+// *********** Immediately Invoked Function Expressions (IIFE) ******************
+/* Reasons for using IIFE
+Encapsulation: IIFE creates a private scope for variables, preventing them from polluting the global scope and avoiding conflicts with other parts of your code.
+Anonymous or Named: You can define IIFE as an anonymous function (without a name) or a named function (with a name). Anonymous IIFE is more common, but named IIFE can be helpful for debugging.
+*/
+(function chai1(){
+    // NAMED IIFE
+    console.log("Hello world!");
+})(); // Function execution immediately after function definition
+// ** NOTE - here ending the execution call with ';' is necessary for interpretor to understand that this fucntion call is over
+
+( () => {
+    // ANONYMOUS IIFE
+    console.log("hello world from arrow function!");
+})() // This also works for arrow functions
---
+++ Basics_2/js_execution_context.js
@@ -0,0 +1,18 @@
+/* NOTES
+
+JS is single threaded in which everything is a process.
+
+********** Java Excecution Context 
+JEC means how javascript executes a .js file, it basically executes the file in two phases which are MEMORY CREATION PHASE and 
+EXECUTION PHASE
+
+Global Excecution Context
+Function Execution Context
+Eval Execution Context
+
+so each file will definitely have Global Excecution Context which contains 'this' means current object, keep this in mind that 
+global object in case of browser is windows object.
+
+
+
+*/
